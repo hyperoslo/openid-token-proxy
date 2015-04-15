1150977a6afa8bc9e864c68162be1a62455d9f10
Tim Kurvers <tim@moonsphere.net>
2015-04-15 21:47:42 +0200

    Spec Token#to_s

--- spec/lib/openid_token_proxy/token_spec.rb
+++ spec/lib/openid_token_proxy/token_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe OpenIDTokenProxy::Token do
+  subject { described_class.new 'access token' }
+
+  describe '#to_s' do
+    it 'returns access token' do
+      expect(subject.to_s).to eq 'access token'
+    end
+  end
 end
